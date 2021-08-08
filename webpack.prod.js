5baaf6066dd028bc133facd0fa467142b1c3e127
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-08-08 04:15:23 -0400

    Project Production ready testing

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const htmlPages = [
+  "about",
+  "cart",
+  "man",
+  "popup",
+  "product",
+  "selection",
+  "woman",
+];
+
+const multipleHtmlOptimizer = htmlPages.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `${name}.html`, // relative path to the HTML files
+    filename: `${name}.html`, // output HTML files
+    minify: {
+      removeAttributeQuotes: true,
+      collapseWhitespace: true,
+      removeComments: true,
+    },
+  });
+});
 
 module.exports = {
   mode: "production",
@@ -12,16 +36,23 @@ module.exports = {
     filename: "[name].[hash].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "index.html",
+        filename: "index.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ].concat(multipleHtmlOptimizer),
+  },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: "man.html",
-      template: "man.html",
-    }),
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
     new CleanWebpackPlugin(),
   ],
   module: {
@@ -33,7 +64,8 @@ module.exports = {
       {
         test: /\.(svg|jpg|gif)$/,
         use: {
-          loader: "file-loader?name=[path][name].[ext]!extract-loader!html-loader",
+          loader:
+            "file-loader?name=[path][name].[ext]!extract-loader!html-loader",
           options: {
             name: "[name].[hash].[ext]",
             outputPath: "imgs",
